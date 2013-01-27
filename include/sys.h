69acbf9a5cdc2b2b3b857d11d695782c938223ec
Karim Allah Ahmed <karim.allah.ahmed@gmail.com>
2013-01-27 22:02:44 +0000

    Increase the size of the initial user space mapping

--- include/sys.h
+++ include/sys.h
@@ -1,7 +1,10 @@
 #ifndef _USER_H
 #define _USER_H
 
+#include <paging.h>
+
 #define USER_START_ADDR (32*1024*1024)
+#define USER_SIZE (PAGE_SIZE * 4)
 #define USER_STACK_ADDR (64*1024*1024)
 
 #endif
